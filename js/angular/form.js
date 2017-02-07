b9170f3ef7624058b96a9456439d788aac9f59b4
yearngo <yearn@ablesms.com>
2017-02-07 18:17:46 +0800

    angular

---
+++ js/angular/form.js
@@ -0,0 +1,25 @@
+﻿var $app = angular.module("MyApp", []);
+var mobile_regexp = /^[5|6|9]\d{7}$/;
+$app.controller("MyCtrl", function ($scope) {
+    $scope.submit = function () {
+        //MyForm.txtEmail.value  獲取單個value值
+        console.log(MyForm.txtEmail.value);
+    };
+});
+//定義香港電話號碼格式
+$app.directive("mobile", function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, elm, attrs, ctrl) {
+            ctrl.$validators.igid = function (modelValue, viewValue) {
+                if (ctrl.$isEmpty(modelValue)) {  //允許為空
+                    return true;
+                }
+                if (mobile_regexp.test(viewValue)) {
+                    return true;
+                }
+                return false;
+            };
+        }
+    };
+});
---
+++ js/angular/photo.js
@@ -0,0 +1,4 @@
+﻿var $app = angular.module("PhotoWall", []);
+$app.controller("PhotoCtrl", function ($scope) {
+    $scope.photo = [{ src: "/images/logo.png", title: "title1" }, { src: "/images/logo.png", title: "title2" }];
+});
