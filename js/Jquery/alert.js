2e66d4cdfb83b541be307ae88142d076248160d9
yearngo <yearn@ablesms.com>
2017-04-13 17:49:07 +0800

    自定義提示

--- js/Jquery/alert.js
+++ js/Jquery/alert.js
@@ -1,144 +1,48 @@
-﻿(function (window) {
-    window.nativeAlert = window.alert;
-    window.alert = function (text, opt) {
-        var useNativeAlert = false;
-        if (opt != null || opt != undefined) {
-            useNativeAlert = opt.nativeAlert || false;
-        }
-        if (useNativeAlert) {
-            window.nativeAlert(text);
-        } else {
-            WinTip(text, opt);
-        }
-    };
-    window.confirm = function (text, opt)
+﻿(function (window, $) {
+    window.myalert = function (text, fixed) {
+        AlertDemo(text,"alert", fixed);
+    }
+    window.myconfirm = function (text, fixed) {
+        AlertDemo(text, "confirm", fixed);
+    }
+}(window, $));
+
+
+function AlertDemo(text, type, fixed) {
+    var $shadow = $("<div>").addClass("shadow-layer");  //遮蓋層
+    var $demo = $("<div>").addClass("myalert");  //主容器
+    var $type = $("<div>").addClass("myalert-typemsg");  //提示類型
+    var $text = $("<div>").addClass("myalert-text").html(text);  //提示內容
+    var $button = $("<div>").addClass("myalert-button");  //按鈕層
+    var $fixedButton = $("<button>").addClass("fixed").html("確定");  //確定按鈕
+
+    $("body").append($demo);
+    $("body").append($shadow);
+    $type.html("提示");
+    $demo.append($type);
+    $demo.append($text);
+    $button.append($fixedButton);
+    $demo.append($button);
+    if (fixed) {
+        $fixedButton.click(function () {
+            $demo.remove();
+            $shadow.remove();
+            fixed();
+        });
+    } else {
+        $fixedButton.click(function () {
+            $demo.remove();
+            $shadow.remove();
+        });
+    }
+    if (type == "confirm")
     {
-        WinTip(text, opt);
-        return true;
-        
+        var $cancelButton = $("<button>").addClass("cancel").html("取消"); //取消按鈕
+        $button.append($cancelButton);
+        $cancelButton.click(function () {
+            $demo.remove();
+            $shadow.remove();
+        });
     }
-}(window));
-
-function WinTip(text, link) {
-    var wWidth = $(window).width();
-    var wHeight = $(window).height();
-    var $myAlert = $(".myalert");
-    $myAlert.css("margin-left", (wWidth - $myAlert.width()) / 2 + "px").css("margin-top", (wHeight - $myAlert.height()) / 2 + "px");
-    $myAlert.find("div.content").html(text);
-    if (link) {
-        if (link.substring(0, 5) === "call:") {
-            $myAlert.find("button.btn-fixed").attr("onClick", link.substring(5));
-        } else if (link.substring(0, 6) == "blank:") {
-            $myAlert.find("button.btn-fixed").attr("href", link.substring(6)).attr("target", "blank");
-        } else {
-            $myAlert.find("button.btn-fixed").attr("href", link);
-        }
-    }
-    $myAlert.css("display", "block");
+    $demo.css("margin-top", -($demo.height() / 2));
 }
-$(window).ready(function () {
-    var $myAlert = $(".myalert");
-    $myAlert.find("button.btn-fixed").click(function () {
-        $myAlert.hide();
-    });
-    $myAlert.find("button.cancel").click(function () {
-        $myAlert.hide();
-    });
-});
-//(function (window) {
-//    window.nativeAlert = window.alert;
-
-//    window.alert = function (text, opt) {
-//        var useNativeAlert = false;
-
-//        if (opt != null || opt != undefined) {
-//            useNativeAlert = opt.nativeAlert || false;
-//        }
-
-//        //if (useNativeAlert) {
-//        //    window.nativeAlert(text);
-//        //} else {
-//        WinTip1(text, opt);
-//        //}
-//    };
-
-//}(window));
-
-//var $ssa = function (id) { return document.getElementById(id); }
-//function WinTip(text, link, mainPanel) {
-//    var win = new WinSize();
-
-//    if (!mainPanel)
-//        mainPanel = "win1";
-
-//    var Tip = $ssa("bg");
-//    Tip.style.width = "100%";
-//    Tip.style.height = "100%";
-//    if (Tip.style.display == "block") {
-//        Tip.style.display = "none";
-//        $ssa(mainPanel).style.display = "none";
-
-//        UnlockScrollbar();
-
-//    } else {
-//        Tip.style.display = "block";
-//        if (text) {
-//            $('#msg-text').html(text);
-//        }
-
-//        if (link) {
-//            if (link.substring(0, 5) === 'call:') {
-//                //$('#msg-link').attr('onClick', 'javascript:window.history.go(-1);');
-//                $('#msg-link').attr('onClick', link.substring(5));
-//            } else
-//                $('#msg-link').attr('onClick', 'location.href="' + link + '"');
-//        }
-
-//        LockScrollbar();
-
-//        $ssa(mainPanel).style.display = "block";
-//    }
-//}
-
-//function WinTip1(text, link) {
-//    WinTip(text, link, "win1");
-//}
-
-//function WinSize() //函数：获取尺寸
-//{
-//    var winWidth = 0;
-//    var winHeight = 0;
-//    if (window.innerWidth)
-//        winWidth = window.innerWidth;
-//    else if ((document.body) && (document.body.clientWidth))
-//        winWidth = document.documentElement.clientWidth
-//    if (window.innerHeight)
-//        winHeight = window.innerHeight;
-//    else if ((document.body) && (document.body.clientHeight))
-//        winHeight = document.body.clientHeight;
-//    if (document.documentElement && document.documentElement.clientHeight && document.documentElement.clientWidth) {
-//        winHeight = document.documentElement.clientHeight;
-//        winWidth = document.documentElement.clientWidth;
-//    }
-//    return { "W": winWidth, "H": winHeight }
-//}
-
-//function LockScrollbar() {
-//    //if (location == top.location) {
-//    $('html, body').css({
-//        'overflow': 'hidden'
-//    });
-//    //}
-//}
-
-//function UnlockScrollbar() {
-//    if (location == top.location) {
-//        $('html, body').css({
-//            'overflow': 'auto'
-//        });
-//    }
-//}
-
-//if (window.location != window.top.location) {
-//    $('html, body').css('overflow', 'hidden');
-//}
