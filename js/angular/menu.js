ca543f8f47033ce61bf0bf6656303b96fba0bff9
yearngo <yearn@ablesms.com>
2017-04-14 18:31:34 +0800

    test

---
+++ js/angular/menu.js
@@ -0,0 +1,28 @@
+﻿var $app = angular.module("MyWall", ['ngAnimate']);
+$app.controller("MyCtrl", function ($scope) {
+    $scope.ishide = false;
+    $scope.menu = [
+    { "Id": 1, "Text": "一級菜單1" },
+    {
+        "Id": 2, "Text": "一級菜單2", "Items": [
+            { "Id": 4, "Text": "二級菜單2-1" },
+            {
+                "Id": 5, "Text": "二級菜單2-2", "Items": [
+                  { "Id": 8, "Text": "三級菜單2-2-1" },
+                  { "Id": 8, "Text": "三級菜單2-2-2" }]
+            }]
+    },
+    {
+        "Id": 3, "Text": "一級菜單3", "Items": [
+              { "Id": 6, "Text": "二級菜單3-1" },
+              { "Id": 7, "Text": "二級菜單3-2" }]
+    }
+    ];
+    $scope.currentMenu = $scope.menu;
+    $scope.clickmenu = function (menu) {
+        $scope.ishide = true;
+        $scope.currentMenu = menu;
+    }
+});
+
+
---
+++ js/angular/test.js
@@ -0,0 +1,46 @@
+﻿// 定义模块  
+var scroll = angular.module('scroll', []);
+// 定义滚动指令  
+scroll.directive('whenScrolled', function () {
+    return function (scope, elm, attr) {
+        // body窗口的滚动加载--需要Jquery  
+        $(window).scroll(function () {
+            //滚动条距离顶部的距离  
+            var scrollTop = $(window).scrollTop();
+            //滚动条的高度  
+            var scrollHeight = $(document).height();
+            //窗口的高度  
+            var windowHeight = $(window).height();
+            if (scrollTop + windowHeight >= scrollHeight) {
+                scope.$apply(attr.whenScrolled);
+            }
+        });
+    };
+});
+
+scroll.controller('Main', ['$scope', '$http',
+    function ($scope, $http) {
+        // 当前页数  
+        $scope.currentPage = 0;
+        // 总页数  
+        $scope.totalPages = 1;
+        // 防止重复加载  
+        $scope.busy = false;
+        // 存放数据  
+        $scope.items = [];
+        // 请求数据方法  
+        $scope.loadMore = function () {
+            if ($scope.currentPage < $scope.totalPages) {
+                $scope.currentPage++;
+                if ($scope.busy) {
+                    return false;
+                }
+                $scope.busy = true;
+                $scope.items.push("111");
+                $scope.items.push("222");
+                $scope.totalPages = 1;
+            }
+        };
+        // 默认第一次加载数据  
+        $scope.loadMore();
+    }]);
